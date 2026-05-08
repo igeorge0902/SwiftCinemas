2d17cf99b041b9a3a835f5744a8d2ee049c8274a
GASPAR, GYORGY <gyorgy.gaspar@lhsystems.com>
2026-05-08 11:54:17 +0200

    chore: remove unused random color generation code and MapView references

--- SwiftLoginScreen/Helpers.swift
+++ SwiftLoginScreen/Helpers.swift
@@ -8,30 +8,6 @@
 
 import UIKit
 
-func generateRandomData() -> [[UIColor]] {
-    let numberOfRows = 20
-    let numberOfItemsPerRow = 15
-
-    // for all rows
-    return (0 ..< numberOfRows).map { _ in
-
-        // for any items in a row
-        (0 ..< numberOfItemsPerRow).map { _ in
-
-            UIColor.randomColor()
-        }
-    }
-}
-
-extension UIColor {
-    class func randomColor() -> UIColor {
-        let hue = CGFloat(arc4random() % 100) / 100
-        let saturation = CGFloat(arc4random() % 100) / 100
-        let brightness = CGFloat(arc4random() % 100) / 100
-
-        return UIColor(hue: hue, saturation: saturation, brightness: brightness, alpha: 1.0)
-    }
-}
 
 extension UIAlertController {
     static func popUp(title: String, message: String) {
